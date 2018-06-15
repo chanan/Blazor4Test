ae82a0fa892e211c8392976d9bed82f4c46423b8
Chanan Braunstein <chanan@bkbunch.com>
2018-06-15 08:10:45 -0600

    Initial commit

---
+++ Blazor4Test/Pages/BootstrapComponentBase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Microsoft.AspNetCore.Blazor;
+using Microsoft.AspNetCore.Blazor.Components;
+
+namespace Blazor4Test.Pages
+{
+    public abstract class BootstrapComponentBase : BlazorComponent
+    {
+        /// <summary>
+        /// A dictionary holding any parameter name/value pairs that do not match
+        /// properties declared on the component.
+        /// </summary>
+        protected IDictionary<string, object> UnknownParameters { get; }
+            = new Dictionary<string, object>();
+
+        public Action<UIMouseEventArgs> OnClick { get; set; }
+
+        /// <inheritdoc />
+        public override void SetParameters(ParameterCollection parameters)
+        {
+            UnknownParameters.Clear();
+
+            foreach (var param in parameters)
+            {
+                if (TryGetPropertyInfo(param.Name, out var declaredPropertyInfo))
+                {
+                    declaredPropertyInfo.SetValue(this, param.Value);
+                }
+                else
+                {
+                    UnknownParameters[param.Name] = param.Value;
+                }
+            }
+
+            StateHasChanged();
+        }
+
+        private bool TryGetPropertyInfo(string propertyName, out PropertyInfo result)
+        {
+            result = GetType().GetProperty(propertyName, BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
+            return result != null;
+        }
+    }
+}
---
+++ Blazor4Test/Pages/DynamicElement.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Blazor;
+using Microsoft.AspNetCore.Blazor.Components;
+using Microsoft.AspNetCore.Blazor.RenderTree;
+
+namespace Blazor4Test.Pages
+{
+    public class DynamicElement : IComponent, IHandleEvent
+    {
+        /// <summary>
+        /// Gets or sets the name of the element to render.
+        /// </summary>
+        public string TagName { get; set; }
+
+        public Action<UIMouseEventArgs> OnClick { get; set; }
+
+        /// <summary>
+        /// Gets or sets the attributes to render.
+        /// </summary>
+        public IReadOnlyDictionary<string, object> Attributes
+        {
+            // The property is only declared for intellisense. It's not used at runtime.
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets the <see cref="Microsoft.AspNetCore.Blazor.ElementRef"/>.
+        /// </summary>
+        public ElementRef ElementRef { get; private set; }
+
+        private RenderHandle _renderHandle;
+        private IDictionary<string, object> _attributesToRender;
+        private RenderFragment _childContent;
+
+        /// <inheritdoc />
+        public void Init(RenderHandle renderHandle)
+        {
+            _renderHandle = renderHandle;
+        }
+
+        /// <inheritdoc />
+        public void SetParameters(ParameterCollection parameters)
+        {
+            _attributesToRender = (IDictionary<string, object>)parameters.ToDictionary();
+            _childContent = GetAndRemove<RenderFragment>(_attributesToRender, RenderTreeBuilder.ChildContent);
+
+            TagName = GetAndRemove<string>(_attributesToRender, nameof(TagName))
+                ?? throw new InvalidOperationException($"No value was supplied for required parameter '{nameof(TagName)}'.");
+
+            // Combine any explicitly-supplied attributes with the remaining parameters
+            var attributesParam = GetAndRemove<IReadOnlyDictionary<string, object>>(_attributesToRender, nameof(Attributes));
+            if (attributesParam != null)
+            {
+                foreach (var kvp in attributesParam)
+                {
+                    if(kvp.Value != null) Console.WriteLine("value: {0}", kvp.Value.GetType().FullName);
+                    if (kvp.Value != null
+                        && _attributesToRender.TryGetValue(kvp.Key, out var existingValue)
+                        && existingValue != null)
+                    {
+                        _attributesToRender[kvp.Key] = existingValue.ToString()
+                            + " " + kvp.Value.ToString();
+                    }
+                    else
+                    {
+                        _attributesToRender[kvp.Key] = kvp.Value;
+                    }
+                }
+            }
+
+            _renderHandle.Render(Render);
+        }
+
+        private static T GetAndRemove<T>(IDictionary<string, object> values, string key)
+        {
+            if (values.TryGetValue(key, out var value))
+            {
+                values.Remove(key);
+                return (T)value;
+            }
+            else
+            {
+                return default;
+            }
+        }
+
+        private void Render(RenderTreeBuilder builder)
+        {
+            builder.OpenElement(0, TagName);
+            foreach (var kvp in _attributesToRender)
+            {
+                builder.AddAttribute(1, kvp.Key, kvp.Value);
+            }
+            builder.AddElementReferenceCapture(2, capturedRef => { ElementRef = capturedRef; });
+            builder.AddContent(3, _childContent);
+            builder.CloseElement();
+        }
+
+        public void HandleEvent(EventHandlerInvoker binding, UIEventArgs args)
+        {
+            binding.Invoke(args);
+        }
+    }
+}
